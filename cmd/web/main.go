13cf246cc32c12c303587b557c623680d9820976
castevet6 <antti.e.manninen@tuni.fi>
2020-12-12 00:24:32 +0200

    Added flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+    "flag"
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
+    // Define flag addr for HTTP address
+    addr := flag.String("addr", ":4000", "HTTP Network Address")
+    flag.Parse()
+    
+    // create info logger
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+    // create error log
+    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
     mux.HandleFunc("/snippet", showSnippet)
@@ -17,7 +28,14 @@ func main() {
     // register fileserver for routes starting with /static
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    log.Println("Starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+    // initialize server struct
+    srv := &http.Server {
+        Addr: *addr,
+        ErrorLog: errorLog,
+        Handler: mux,
+    }
+
+    infoLog.Printf("Starting server on %s\n", *addr)
+    err := srv.ListenAndServe()
+    errorLog.Fatal(err)
 }
