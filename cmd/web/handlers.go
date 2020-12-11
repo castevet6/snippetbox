2b3072408acc9b1c76c2bb53a86d3dbe178dd5e5
castevet6 <antti.e.manninen@tuni.fi>
2020-12-11 23:08:07 +0200

    Added ui templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,17 +2,42 @@ package main
 
 import (
     "fmt"
+    "html/template"
+    "log"
     "net/http"
     "strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+    // prevent catch all
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
     }
 
-    w.Write([]byte("Hello from SnippetBox"))
+    // init a slice containing two template files
+    files := []string{
+        "./ui/html/home.page.tmpl",
+        "./ui/html/footer.partial.tmpl",
+        "./ui/html/base.layout.tmpl",
+    }
+    
+    // use parse home page template, return error or page
+    // note variadic param files...
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    // write tmpl content to response body with Execute(), no w.Write needed:wq
+    // no dynamic data (last param of Execute)
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -34,5 +59,3 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 
     w.Write([]byte("Create new snippet..."))
 }
-
-
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,12 @@ func main() {
     mux.HandleFunc("/snippet", showSnippet)
     mux.HandleFunc("/snippet/create", createSnippet)
 
+    // create file server for static files
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+    // register fileserver for routes starting with /static
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
     log.Println("Starting server on :4000")
     err := http.ListenAndServe(":4000", mux)
     log.Fatal(err)
